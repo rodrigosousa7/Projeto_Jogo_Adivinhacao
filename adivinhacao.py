c855c93c8ee2f1d02e6aaec868498be33f066b5c
rodrigosousa7 <rodrigo.sousa@email.com>
2023-05-12 13:21:32 -0300

    inclusao de sistema de rodadas

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,21 +7,26 @@ print("Bem vindo ao jogo de adivinhacao!")
 print("*********************************")
 
 NUMERO_SECRETO = 77
+TOTAL_DE_TENTATIVAS = 3
+RODADA = 1
 
-chute = int(input("Digite o seu numero:"))
+while RODADA <= TOTAL_DE_TENTATIVAS:
+    print("Tentativa", RODADA, "de", TOTAL_DE_TENTATIVAS)
+    chute = int(input("Digite o seu numero:"))
+    print("Voce digitou", chute)
 
-print("Voce digitou", chute)
+    ACERTOU = chute == NUMERO_SECRETO
+    NUMERO_MAIOR = chute > NUMERO_SECRETO
+    NUMERO_MENOR = chute < NUMERO_SECRETO
 
-ACERTOU = chute == NUMERO_SECRETO
-NUMERO_MAIOR = chute > NUMERO_SECRETO
-NUMERO_MENOR = chute < NUMERO_SECRETO
+    if ACERTOU:
+        print("Parabéns, voce acertou")
+    else:
+        if NUMERO_MAIOR:
+            print("Seu chute foi maior que o número secreto")
+        elif NUMERO_MENOR:
+            print("Seu chute foi menor que o número secreto")
 
-if ACERTOU:
-    print("Parabéns, voce acertou")
-else:
-    if NUMERO_MAIOR:
-        print("Seu chute foi maior que o número secreto")
-    elif NUMERO_MENOR:
-        print("Seu chute foi menor que o número secreto")
+    RODADA = RODADA + 1
 
 print("Fim do jogo")
