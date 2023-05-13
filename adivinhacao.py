58dc638b4f3914bce75d0e4661237d013b2eea82
rodrigosousa7 <rodrigo.sousa@email.com>
2023-05-13 00:45:29 -0300

    Refaturacao do codigo e melhorias

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,21 +7,36 @@ print("Bem vindo ao jogo de adivinhacao!")
 print("*********************************")
 
 NUMERO_SECRETO = 77
+TOTAL_DE_TENTATIVAS = 3
+RODADA = 1
 
-chute = int(input("Digite o seu numero:"))
+for RODADA in range(1, TOTAL_DE_TENTATIVAS + 1):
+    print(f"Tentativa {RODADA} de {TOTAL_DE_TENTATIVAS}")
+    print()
 
-print("Voce digitou", chute)
+    chute = int(input("Digite um número entre 1 e 100: "))
+    print("Voce digitou --->", chute)
+    print()
 
-ACERTOU = chute == NUMERO_SECRETO
-NUMERO_MAIOR = chute > NUMERO_SECRETO
-NUMERO_MENOR = chute < NUMERO_SECRETO
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100!")
+        print()
+        continue
 
-if ACERTOU:
-    print("Parabéns, voce acertou")
-else:
-    if NUMERO_MAIOR:
-        print("Seu chute foi maior que o número secreto")
-    elif NUMERO_MENOR:
-        print("Seu chute foi menor que o número secreto")
+    ACERTOU = chute == NUMERO_SECRETO
+    NUMERO_MAIOR = chute > NUMERO_SECRETO
+    NUMERO_MENOR = chute < NUMERO_SECRETO
+
+    if ACERTOU:
+        print("Parabéns, voce acertou!")
+        print()
+        break
+    else:
+        if NUMERO_MAIOR:
+            print("Seu chute foi maior que o número secreto")
+            print()
+        elif NUMERO_MENOR:
+            print("Seu chute foi menor que o número secreto")
+            print()
 
 print("Fim do jogo")
