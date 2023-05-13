171b005745b66f0c8ece96996d6949a3c5865ce0
rodrigosousa7 <rodrigo.sousa@email.com>
2023-05-13 00:33:39 -0300

    refaturacao do codigo

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,7 +2,6 @@
 Projeto de jogo de adivinhação
 """
 
-
 print("*********************************")
 print("Bem vindo ao jogo de adivinhacao!")
 print("*********************************")
@@ -11,23 +10,33 @@ NUMERO_SECRETO = 77
 TOTAL_DE_TENTATIVAS = 3
 RODADA = 1
 
-while RODADA <= TOTAL_DE_TENTATIVAS:
-    print("Tentativa", RODADA, "de", TOTAL_DE_TENTATIVAS)
-    chute = int(input("Digite o seu numero:"))
-    print("Voce digitou", chute)
+for RODADA in range(1, TOTAL_DE_TENTATIVAS + 1):
+    print(f"Tentativa {RODADA} de {TOTAL_DE_TENTATIVAS}")
+    print()
+
+    chute = int(input("Digite um número entre 1 e 100: "))
+    print("Voce digitou --->", chute)
+    print()
+
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100!")
+        print()
+        continue
 
     ACERTOU = chute == NUMERO_SECRETO
     NUMERO_MAIOR = chute > NUMERO_SECRETO
     NUMERO_MENOR = chute < NUMERO_SECRETO
 
     if ACERTOU:
-        print("Parabéns, voce acertou")
+        print("Parabéns, voce acertou!")
+        print()
+        break
     else:
         if NUMERO_MAIOR:
             print("Seu chute foi maior que o número secreto")
+            print()
         elif NUMERO_MENOR:
             print("Seu chute foi menor que o número secreto")
-
-    RODADA = RODADA + 1
+            print()
 
 print("Fim do jogo")
