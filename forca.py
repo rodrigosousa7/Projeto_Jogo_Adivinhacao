02f0883e60b99c0c0d8e6e4d61bfed0a5bba035a
rodrigosousa7 <rodrigo.sousa@email.com>
2023-05-14 17:31:31 -0300

    novo jogo acrescentado, ainda em desenvolvimento

--- forca.py
+++ forca.py
@@ -1,10 +1,15 @@
 """
 Projeto de jogo de forca
 """
+def jogar():
+    """
+    função para executar o jogo
+    """
+    print("*********************************")
+    print("** Bem vindo ao jogo de FORCA! **")
+    print("*********************************\n")
 
-print("*********************************")
-print("***Bem vindo ao jogo de FORCA!***")
-print("*********************************")
+    print("Fim do jogo.\n")
 
-print("Fim do jogo.")
-print()
+if __name__ == "__main__":
+    jogar()
